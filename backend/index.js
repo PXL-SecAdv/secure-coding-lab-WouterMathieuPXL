7ed5fe130267de1d358a4e8c3a36b7c0bf946c2a
WouterMathieuPXL <Wouter.Mathieu@student.pxl.be>
2024-04-16 20:05:20 +0200

    CORS 2

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const pool = new pg.Pool({
 console.log("Connecting...:")
 
 const corsOptions = {
-    origin: 'http://192.168.159.130',
+    origin: 'http://localhost',
     optionsSuccessStatus: 200 // legacy browser support (IE11, various SmartTVs)
 };
 
